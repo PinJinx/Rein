3c068f42ded9ed1fe6dba6111be7bbb05d58a240
XADE <notadevel@gmail.com>
2026-03-05 20:31:24 +0530

    chore: remove redundancy from shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    nodejs_24
+let
+  sharedLibs = with pkgs; [
     gtk3
     nss
     nspr
@@ -12,18 +11,14 @@ pkgs.mkShell {
     xorg.libX11
     xorg.libXext
   ];
+in
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.nodejs_24
+  ] ++ sharedLibs;
 
   shellHook = ''
-    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
-      pkgs.gtk3
-      pkgs.nss
-      pkgs.nspr
-      pkgs.alsa-lib
-      pkgs.libglvnd
-      pkgs.xorg.libXtst
-      pkgs.xorg.libX11
-      pkgs.xorg.libXext
-    ]}:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath sharedLibs}:$LD_LIBRARY_PATH
     
     # Alias for starting the application
     alias start="npm run start:server & npm run dev"
