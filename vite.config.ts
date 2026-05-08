d0b94c30deb800907eb6acf85ef5af53cea7d4fe
pinjinx <rohithkpradeep@gmail.com>
2026-05-08 20:04:12 +0530

    Vite 8 update and dependency fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import viteReact from "@vitejs/plugin-react"
 import { nitro } from "nitro/vite"
 import { defineConfig } from "vite"
-import viteTsConfigPaths from "vite-tsconfig-paths"
 import serverConfig from "./src/server-config.json"
 import { createWsServer } from "./src/server/websocket"
 
@@ -31,11 +30,6 @@ const config = defineConfig({
 		},
 		devtools(),
 		nitro(),
-		// this is the plugin that enables path aliases
-		viteTsConfigPaths({
-			projects: ["./tsconfig.json"],
-		}),
-
 		tanstackStart(),
 		viteReact(),
 	],
